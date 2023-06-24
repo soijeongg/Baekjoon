6ad679f7bdc6bc2d46c20a5cf7a995b195f5dbc6
soijeongg <136438441+soijeongg@users.noreply.github.com>
2023-06-25 08:21:25 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -9,8 +9,9 @@ for i in word_list:
 #이러면 하나씩 갯수를 셀 수 있다 이걸 새 list에 넣기 
 #이러면 word와 
 #여기서 제일 큰거 찾기,근데 2개이상이면 ? 출력
-if new_list.count(max(new_list))>=2:
+max_num = max(new_list)
+if new_list.count(max_num)>=2:
     print("?")
 #제일 큰거 하나 나오면 그걸 list에서 위치찾아 다시 word에서 찾기
 else:
-    print(word_list[new_list.index(max(new_list))])
+    print(word_list[new_list.index(max_num)])
