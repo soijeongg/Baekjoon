78a68a4118ebb5268da811e13e7437a431327fd2
soijeongg <136438441+soijeongg@users.noreply.github.com>
2023-07-17 17:29:47 +0900

    [Bronze II] Title: 트럭 주차, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.py
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.py
@@ -1,34 +1,26 @@
-
+#리스트 만들어서풀기
 a, b, c = map(int, input().split())
-
-time_list_s = []
-time_list_d = []
-
+time_list=[0]*100
 for _ in range(3):
     start_time, end_time = map(int, input().split())
-    time_list_s.append(start_time)
-    time_list_d.append(end_time)
+    for i in range (start_time,end_time):
+        time_list[i]+=1
+sum =0
+for n in time_list:
 
-max_d_time = max(time_list_d)
-min_time = min(time_list_s)
+    if n==3:
 
-cnt_car = 0
-cnt_list = []
+        sum+=n*c
 
-for i in range(min_time, max_d_time):
-    for j in range(3):
-        if time_list_s[j] <= i < time_list_d[j]:
-            cnt_car += 1
-    cnt_list.append(cnt_car)
-    cnt_car = 0
+    elif n==2:
 
-total_fee = 0
-for count in cnt_list:
-    if count == 3:
-        total_fee += c * 3
-    elif count == 2:
-        total_fee += b * 2
-    elif count == 1:
-        total_fee += a
+        sum+= n*b
 
-print(total_fee)
+    elif n==1:
+        sum+= n*a
+
+    
+    else:
+        pass
+
+print(sum)
