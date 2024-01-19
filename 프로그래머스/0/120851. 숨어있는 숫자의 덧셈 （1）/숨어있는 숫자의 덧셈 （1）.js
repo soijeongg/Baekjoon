29814dfd353143c60aecd4e3827d40825806b4b4
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-01-19 18:58:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+    var answer = 0;
+    let num =[];
+    //js에서도 정수인지 체크하는 ParseInt 아니면 Nan
+    let my_string_list = Array.from(my_string,Number);
+    for (let i=0;i<my_string_list.length;i++){
+        if(parseInt(my_string_list[i])){
+            num.push(my_string_list[i]);
+        }
+    }
+    for(let j=0;j<num.length;j++){
+        answer+=num[j];
+    }
+    return answer;
+}
