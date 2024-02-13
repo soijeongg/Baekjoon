a9573442a8422cf5cd604b68c6f57d672caaa2ae
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-02-13 11:32:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -2,14 +2,12 @@ function solution(absolutes, signs) {
     var answer = 0;
     for(let i=0;i<absolutes.length;i++){
         if(signs[i]==true){
-            absolutes[i]=absolutes[i];
+          answer+=absolutes[i];
         }else {
-             absolutes[i]=-absolutes[i];
+              answer-=absolutes[i];
         }
     }
-     for(let i=0;i<absolutes.length;i++){
-        answer+=absolutes[i]
-     }
+    
     return answer;
  
 }
