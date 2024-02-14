8707651585e7904ee52830059faa4e78b32af63a
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-02-14 10:05:35 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.79 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,16 +2,16 @@ function solution(s) {
     var answer = [];
     let count = [];
     s= s.split("")
-   s.forEach((char, index) => {
-        if (!(char in count)) {
+   s=  s.forEach((e,v)=>{
+        if (!count.includes(e)) {
             answer.push(-1);
-        } else {
-            answer.push(index - count[char]);
+        }else{
+            answer.push(v-count.lastIndexOf(e))
         }
-        count[char] = index;
-    });
-
+       count.push(e);
+   })
     return answer;
 }
 
 //그러니까 중복된 글자가 없으면 -1을 제시하고 중복된 글자가 있으면 자신과 얼마나 떨어져 있는지 더 가까운애와의 차이를 구하여라
+//이 문제에서 중요한거는 어떻게 하면 중복된 값을 계속 바꾸느냐
