a319c97111c39b9c28420812c0eabfce3ed67f9b
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-02-16 10:12:20 +0900

    [level 1] Title: 시저 암호, Time: 1.39 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,12 +1,12 @@
 function solution(s, n) {
     var answer = String(s).split("");
-    let alphaA = 'abcdefghijklmnopqrstuvwxyz';
-    let alphab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let alphaA = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+    let alphab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
     answer = answer.map((e,v)=> {
         if(alphaA.includes(e)){
-            return alphaA[(alphaA.indexOf(e)+n)%26]
+            return alphaA[(alphaA.indexOf(e)+n)]
         }else if(alphab.includes(e)){
-            return alphab[(alphab.indexOf(e)+n)%26]
+            return alphab[(alphab.indexOf(e)+n)]
         }else{
             return e
         }
