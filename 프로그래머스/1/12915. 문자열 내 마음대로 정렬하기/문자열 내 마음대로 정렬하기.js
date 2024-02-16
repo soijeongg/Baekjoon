1b69ed50319d6c23edec46aa7af4892b99dd5d07
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-02-16 15:26:23 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.85 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,20 +1,23 @@
-function solution(strings, n) {
-    //그러니까 n이 각각의 인덱스로 들어가 배열을 만든다
-    //이글자가 어디서 왔는지가 중요한거야?? 그럼 뒤에 숫자 0,1,2붙이고 알파벳으로 정렬한 다음 
-    
-    
-    strings.sort((a, b) => {
-    let alphaA = a[n];
-    let alphaB = b[n]
-        if (alphaA === alphaB) {
-            return a.localeCompare(b)
-        } else {
-            return alphaA.localeCompare(alphaB);
-        }
 
-  
-});
-    
-   
-    return strings;
+function solution(strings, n) {
+
+
+  strings.sort((a, b) => {
+    let alphaA = a[n];
+    let alphaB = b[n];
+    if (alphaA === alphaB) {
+      return a.localeCompare(b);
+    } else {
+      return alphaA.localeCompare(alphaB);
+    }
+  });
+
+  return strings;
 }
+
+//sort를 하는데 인덱스의 n을 기준으로 오름차순 정렬이여서 스트링의 각 수의 n번째 인덱스를 가진거 가져옴
+//만약 이 둘이 다르면 이 둘을 기준으로 localeCompare 정렬
+//localeCompare는 앞의 문자열(참조문자열)이 ()안의 문자열보다 앞에 나오는것보다 늦게 나오면 1, 반대면 -1
+//그리고 만약 같다면 그냥 a,b 전체 비교 
+//그리고 나온 숫자들을 이용해 정렬
+//결국 두수를 -1,1이 둘로 바꿔서 숫자들을 오름차순 하는거
