c589cdebf33520dcdbc5dd7fbf70934aa90d280d
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-02-16 09:24:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,5 +1,6 @@
-function solution(n) {
-    var answer = n.toString(3).split("").reverse().join("");
-    answer = parseInt(answer,3);
-    return answer;
-}
+ function solution(n) {
+   var answer = n.toString(3).split("").reverse().join("");
+      answer = parseInt(answer, 3);
+   
+   return answer;
+ }
