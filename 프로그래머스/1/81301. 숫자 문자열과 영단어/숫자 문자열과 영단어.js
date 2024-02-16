4cfdc43b3e8d75031f4b570710a793ddb67c0ccd
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-02-16 15:25:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,9 +1,26 @@
 function solution(s) {
-    var answer = 0;
-    let number = ['zero','one','two','three','four','five','six','seven','eight','nine']
-   number.forEach((e, i) => {
-        s = s.split(e).join(i)    
-    })
+  let number = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  number.forEach((e, i) => {
+    s = s.split(e).join(i);
+  });
 
-    return Number(s);
+  return Number(s);
 }
+
+//먼저 숫자에 대응하는 영단어의 배열을 만듬
+//이 영어배열은 인덱스에 해당하는 영어가 값으로 들어있기 때문에 사용
+//이배열을 foreach로 돌면서 s를 element기준으로 나누고 기준이 되는 문자는 사라짐 그리고 
+//그리고 이 두 배열을 그 수의 인덱스를 가지고외서 붙음
+//join은 안의 문자열을 붙잃때마다 넣음
+//만약 s에 기준이 되는 문자열인 e가 없다면 그냥 넘어감
