51dd6ef5104463e93ccce76baf3f999dd1c0baa0
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-03-07 09:32:49 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,10 @@
+t = int(input())
+for i in range(t):
+    h, w, n = map(int, input().split( ))
+    
+    floor = n % h 
+    room_line = (n // h) + 1
+    if floor == 0:
+        floor = h
+        room_line -= 1
+    print(floor * 100 + room_line)
