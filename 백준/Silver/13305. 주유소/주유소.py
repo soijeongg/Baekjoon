2f3377c93d90d1b2f96a6282f658e9e2fdf7654e
정소이 <136438441+soijeongg@users.noreply.github.com>
2024-05-08 17:36:55 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46160 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,21 +2,21 @@ city_count = int(input())
 doro = []
 money = []
 
-doro = list(map(int,input().split())) 
+doro = list(map(int,input().split())) # 올바른 방법으로 int 형으로 변환하여 추가
 money = list(map(int,input().split()))
+  # 올바른 방법으로 int 형으로 변환하여 추가
 
-
-# 주유 비용과 거리가 담긴 리스트
+# 주유 비용과 거리가 담긴 리스트가 제대로 만들어졌습니다.
 
 
 oil = money[0]
 s = oil * doro[0]
 
-for k in range(1, city_count-1): #마지막은 빼야 됨
+for k in range(1, city_count-1):
     if oil > money[k]:
         oil = money[k]
         s += oil * doro[k]
     else:
-        s += oil * doro[k]  
+        s += oil * doro[k]  # 비교 결과에 상관없이 해당 줄은 항상 실행되어야 합니다.
 
 print(s)
